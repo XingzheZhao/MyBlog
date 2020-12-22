e62891793ed6584e4dd8133ed6913034e33bdd8e
Xingzhe Zhao <samzhao0528@gmail.com>
2020-12-22 15:16:30 -0800

    email and password reset

--- my_blog/myblog/__init__.py
+++ my_blog/myblog/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 import os
 
 load_dotenv()
@@ -19,5 +20,12 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'    # for login required page
 login_manager.login_message_category = 'info'   # ! login messagecolor blue
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.getenv("EMAIL_USER")
+# app.config['MAIL_USERNAME'] = os.environ.get("EMAIL_USER")
+app.config['MAIL_PASSWORD'] = os.getenv("EMAIL_PASS")
+mail = Mail(app)
 
 from myblog import routes
